0fec97214d259d064f007cbf6ce5ee1b56942825
Joaquin <joaquintalice@gmail.com>
2024-02-04 23:30:23 -0300

    feat: ProductRepository interface

---
+++ Distributor.Core/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Distributor.Core.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        bool SaveChanges();
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<int> CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
