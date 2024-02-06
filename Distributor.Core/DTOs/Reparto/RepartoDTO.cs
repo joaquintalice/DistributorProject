844c5bd1024dab3f6949c449e28bcd0409d5e0f8
Joaquin <joaquintalice@gmail.com>
2024-02-05 23:59:14 -0300

    feat: Reparto DTO

---
+++ Distributor.Core/DTOs/Reparto/RepartoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Distributor.Core.DTOs.Reparto
+{
+    public class RepartoDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
