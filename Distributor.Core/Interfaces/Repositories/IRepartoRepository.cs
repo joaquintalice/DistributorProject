e508159580882b67b938f6c16aa58108cdedc16a
Joaquin <joaquintalice@gmail.com>
2024-02-05 23:59:46 -0300

    feat: Reparto repository interface

---
+++ Distributor.Core/Interfaces/Repositories/IRepartoRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Distributor.Core.Interfaces.Repositories
+{
+    public interface IRepartoRepository
+    {
+        Task<Reparto> GetRepartoByIdAsync(int repartoId);
+        Task<IReadOnlyList<Reparto>> GetRepartosAsync();
+        Task<Reparto> CreateRepartoAsync(Reparto reparto);
+        Task<Reparto> UpdateRepartoAsync(Reparto reparto);
+        Task<Reparto> DeleteRepartoAsync(int id);
+    }
+}
